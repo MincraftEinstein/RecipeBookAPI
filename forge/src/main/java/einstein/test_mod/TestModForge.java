4df3c93099d48eb3f0a6c7a3c1741c04c151b4b0
MincraftEinstein <70532030+MincraftEinstein@users.noreply.github.com>
2023-10-27 21:07:35 -0400

    Added forge key pressed event + fixed menu screen on forge

--- forge/src/main/java/einstein/test_mod/TestModForge.java
+++ forge/src/main/java/einstein/test_mod/TestModForge.java
@@ -1,13 +1,28 @@
 package einstein.test_mod;
 
 import einstein.recipebook_api.RecipeBookAPIForge;
+import einstein.test_mod.networking.OpenMenuKeyPressedC2SPacket;
+import einstein.test_mod.screens.TestScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.*;
 
 @Mod(TestMod.MOD_ID)
 public class TestModForge {
+
+    public static final SimpleChannel INSTANCE = ChannelBuilder.named(TestMod.loc("main"))
+            .networkProtocolVersion(1)
+            .clientAcceptedVersions(Channel.VersionTest.exact(1))
+            .serverAcceptedVersions(Channel.VersionTest.exact(1))
+            .simpleChannel();
 
     public TestModForge() {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -20,5 +35,17 @@ public class TestModForge {
                         event.register(supplier.get())
                 )
         );
+        modEventBus.addListener((FMLCommonSetupEvent event) -> {
+            INSTANCE.messageBuilder(OpenMenuKeyPressedC2SPacket.class, NetworkDirection.PLAY_TO_SERVER).decoder(OpenMenuKeyPressedC2SPacket::decode).encoder(OpenMenuKeyPressedC2SPacket::encode).consumerMainThread(OpenMenuKeyPressedC2SPacket::handle).add();
+        });
+        modEventBus.addListener((FMLClientSetupEvent event) -> {
+            MenuScreens.register(TestMod.TEST_MENU.get(), TestScreen::new);
+        });
+        MinecraftForge.EVENT_BUS.addListener((InputEvent.Key event) -> {
+            Minecraft minecraft = Minecraft.getInstance();
+            if (minecraft.level != null && event.getKey() == TestMod.OPEN_TEST_MENU.getKey().getValue()) {
+                INSTANCE.send(new OpenMenuKeyPressedC2SPacket(), PacketDistributor.SERVER.noArg());
+            }
+        });
     }
 }
---
+++ forge/src/main/java/einstein/test_mod/networking/OpenMenuKeyPressedC2SPacket.java
@@ -0,0 +1,40 @@
+package einstein.test_mod.networking;
+
+import einstein.test_mod.menus.TestMenu;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraftforge.event.network.CustomPayloadEvent;
+import org.jetbrains.annotations.Nullable;
+
+public record OpenMenuKeyPressedC2SPacket() {
+
+    public static OpenMenuKeyPressedC2SPacket decode(FriendlyByteBuf buf) {
+        return new OpenMenuKeyPressedC2SPacket();
+    }
+
+    public static void encode(OpenMenuKeyPressedC2SPacket packet, FriendlyByteBuf buf) {
+    }
+
+    public static void handle(OpenMenuKeyPressedC2SPacket packet, CustomPayloadEvent.Context context) {
+        Player player = context.getSender();
+        if (player != null) {
+            player.openMenu(new MenuProvider() {
+
+                @Override
+                public Component getDisplayName() {
+                    return Component.literal("Test Menu");
+                }
+
+                @Nullable
+                @Override
+                public AbstractContainerMenu createMenu(int id, Inventory inventory, Player player) {
+                    return new TestMenu(id, inventory);
+                }
+            });
+        }
+    }
+}
